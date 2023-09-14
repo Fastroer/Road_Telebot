ac68aa661305b70f0b0b3e5b9def69f2fa677ad1
Fastroer <fastroer2018@gmail.com>
2023-09-14 14:04:42 +0300

    Gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-db_user = 'postgres'
-db_password = '1qazxswedrT'
-db_name = 'drivers_and_cars'
-db_host = 'localhost'
-db_port = 5432
-
-BOT_TOKEN = '6587239035:AAEmg9YQDMYLwGRLWJVYP2Up8nSs_UeNk1o'
